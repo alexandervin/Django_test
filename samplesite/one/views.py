6fcd07ea54708204a26aa30438f84c0abb5ceed9
AlexanderVin <alexandr.vinitsky@gmail.com>
2020-04-30 14:50:27 +0300

    add_30-04

--- samplesite/one/views.py
+++ samplesite/one/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-from django.http import  HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 
 # Create your views here.
 def show(request):
-    return  HttpResponse('Hello Django!!!!!!!!!!')
-
-
+    return HttpResponse('Hello ----------- Django!!!!!!!!!!')
