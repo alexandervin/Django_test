8352eb968cdadee389285f263c955235d6d445ae
AlexanderVin <alexandr.vinitsky@gmail.com>
2020-05-04 12:00:13 +0300

    add_4/3_05

--- samplesite/one/models.py
+++ samplesite/one/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class ToDoItem(models.Model):
+
+    text = models.CharField(max_length=250)
+    done = models.BooleanField()
--- samplesite/samplesite/settings.py
+++ samplesite/samplesite/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'one',
+    #'one',
     'debug_toolbar',
+    'rest_framework',
+    'one.apps.OneConfig',
+
 ]
 
 MIDDLEWARE = [
